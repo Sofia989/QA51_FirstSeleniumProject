e2dc51c2f538b1c04f90232e64e605e3ddb63bca
Sofia989 <noichl.sofiya@gmail.com>
2025-07-03 16:07:11 +0200

    Added parameterization to registration test

--- src/main/java/com/ait/tests/FindElementTests.java
+++ src/main/java/com/ait/tests/FindElementTests.java
@@ -54,5 +54,73 @@ public void tearDown(){
         WebElement text2 = driver.findElement(By.partialLinkText("work"));
         System.out.println(text2.getText());
     }
+    @Test
+    public void findelementByCSSSelector(){
+
+        WebElement title= driver.findElement(By.cssSelector("h1"));
+        System.out.println(title.getText());
+        driver.findElements(By.cssSelector("#city"));
+
+        WebElement search = driver.findElement(By.cssSelector(".navigation-link"));
+        System.out.println(search.getText());
+
+        //[key=`value`]
+        driver.findElement(By.cssSelector("[href='/search']"));
+
+        //* -contains
+        driver.findElement(By.cssSelector("[class*='offers']"));
+
+        driver.findElement(By.cssSelector("[class^='special']"));
+        driver.findElement(By.cssSelector("[class$='container']"));
+        //составные
+
+        //span[contains(text(),'Latest')]
+
+        driver.findElement(By.cssSelector(".search-container>.subtitle"));
+        driver.findElement(By.cssSelector(".input-container #dates "));
+
+        WebElement login = driver.findElement(By.cssSelector(".navigation-link:nth-child(6)"));
+        System.out.println(login.getText());
+    }
+
+    @Test
+    public void findElementByXpath(){
+        ////*[@attr='value']
+
+        WebElement ul = driver.findElement(By.xpath("//h1"));
+
+
+        driver.findElements(By.xpath("//input[@id='city']"));
+         //className
+        WebElement search = driver.findElement(By.xpath("//*[@class='navigation-link']"));
+
+
+        WebElement searchLink = driver.findElement(By.xpath("//*[@href='/search']"));
+        System.out.println(searchLink.getText());
+
+        //^^start
+
+        //driver.findElement(By.cssSelector("[class^='special']"));
+        driver.findElement(By.xpath("//*[starts-with(@class,'special')]"));
+
+        //contains
+        WebElement feedback = driver.findElement(By.xpath("//span[contains(text(),'Latest')]"));
+        System.out.println(feedback.getText());
+
+       driver.findElement(By.xpath("//*[@class='logo']/img[@alt='logo']"));
+
+        driver.findElement(By.xpath("//*[@class='search-container']/*[@class='subtitle']"));
+
+        driver.findElement(By.xpath("//*[@class='input-container']//input[@id='dates']"));
+
+        WebElement login = driver.findElement(By.xpath("//a[6]"));
+        System.out.println(login.getText());
+
+        driver.findElement(By.xpath("//*[contains(.,'Latest')]"));
+        driver.findElement(By.xpath("//*[.=' Latest feedback from our customers ']"));
+
 
     }
+
+    }
+    //div>a - css           //xpath>//div/a    //cssSelector >div a          //xpath >//div//a
